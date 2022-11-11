6e395f67f0a0ac6522787e301004c9e714802ed8
khak1s <27432909+Kha-kis@users.noreply.github.com>
2022-11-11 10:40:26 -0600

    updated logic

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -2,15 +2,22 @@
 import config
 from urllib.parse import urlsplit
 from qbittorrentapi import Client
+
 client = Client(host=(config.host), username=(config.username), password=(config.password))
 count = 0
+unregistered = [
+'This torrent does not exist',
+'Unregistered torrent',
+'002: Invalid InfoHash, Torrent not found',
+'Torrent is not authorized for use on this tracker']
 
 for torrent in client.torrents.info():
-    for status in torrent.trackers:
-        if ('This torrent does not exist' or 'Unregistered torrent' or '002: Invalid InfoHash, Torrent not found' or 'Torrent is not authorized for use on this tracker') in status.msg:
-            count = count + 1
-            tracker = urlsplit(torrent.tracker)
-            print(torrent.name,' ',status.msg,' ',tracker.netloc)
-            client.torrents_add_tags(tags=(config.tagname),torrent_hashes=(torrent.hash))
+    for tracker in torrent.trackers:
+        for msg in unregistered:
+            if msg in tracker.msg:
+                count = count + 1
+                tracker_short = urlsplit(tracker.url)
+                print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
+                client.torrents_add_tags(tags=(config.tagname),torrent_hashes=(torrent.hash))
 
-print('Torrents Tagged = ', ' ', count)
+print('Torrents Tagged = ', count)
