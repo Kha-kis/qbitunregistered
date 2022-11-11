276cd85f6a47f3b92a27fdbfe0e23c2a4b963008
khak1s <27432909+Kha-kis@users.noreply.github.com>
2022-11-11 15:00:04 -0600

    updated to add support for other errors with the message created as a tag

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -4,20 +4,25 @@ from urllib.parse import urlsplit
 from qbittorrentapi import Client
 
 client = Client(host=(config.host), username=(config.username), password=(config.password))
-count = 0
 unregistered = [
 'This torrent does not exist',
 'Unregistered torrent',
 '002: Invalid InfoHash, Torrent not found',
-'Torrent is not authorized for use on this tracker']
+'Torrent is not authorized for use on th tracker',
+'Torrent not found',
+'Torrent not registered with this tracker.',
+'Unregistered torrent',
+'unregistered torrent',
+'unregistered'
+]
 
 for torrent in client.torrents.info():
     for tracker in torrent.trackers:
-        for msg in unregistered:
-            if msg in tracker.msg:
-                count = count + 1
-                tracker_short = urlsplit(tracker.url)
-                print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
-                client.torrents_add_tags(tags=(config.tagname),torrent_hashes=(torrent.hash))
-
-print('Torrents Tagged = ', count)
+        if tracker.msg in unregistered and (tracker.status == 4):
+                    tracker_short = urlsplit(tracker.url)
+                    print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
+                    client.torrents_add_tags(tags=(config.tagname),torrent_hashes=(torrent.hash))
+        elif tracker.msg != 'This torrent is private' and (tracker.status == 4):
+                    tracker_short = urlsplit(tracker.url)
+                    print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
+                    client.torrents_add_tags(tags=(tracker.msg),torrent_hashes=(torrent.hash))
