618362d82fe3335b043cd4ec7697736ee4be2d14
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-07 08:51:59 -0500

    Update qbitunregistered.py
    added comments to the code

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
+
 import config
 from urllib.parse import urlsplit
 from qbittorrentapi import Client
 
-client = Client(host=(config.host), username=(config.username), password=(config.password))
+# Connect to qbittorrent client
+client = Client(host=config.host, username=config.username, password=config.password)
+
+# List of unregistered tracker messages
 unregistered = config.unregistered
 
+# Dictionary to store file paths and their associated hashes
 torrent_file_paths = {}
+
+# Iterate through all the torrents
 for torrent in client.torrents.info():
+    
+    # Store the hashes in the torrent_file_paths dictionary
     if torrent.save_path not in torrent_file_paths:
         torrent_file_paths[torrent.save_path] = [torrent.hash]
     else:
         torrent_file_paths[torrent.save_path].append(torrent.hash)
 
+    # Initialize unregistered_count to 0
     unregistered_count = 0
+
+    # Check trackers for unregistered messages
     for tracker in torrent.trackers:
         if tracker.msg in unregistered and tracker.status == 4:
             unregistered_count += 1
             tracker_short = urlsplit(tracker.url)
-            print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
+            print(torrent.name, ' ', tracker.msg, ' ', tracker_short.netloc)
 
+    # Add tags based on unregistered_count
     if unregistered_count > 0:
         if len(torrent_file_paths[torrent.save_path]) > 1:
             client.torrents_add_tags(tags=["unregistered:crossseeding"], torrent_hashes=[torrent.hash])
@@ -27,8 +40,11 @@ for torrent in client.torrents.info():
             client.torrents_add_tags(tags=["unregistered"], torrent_hashes=[torrent.hash])
         continue
 
+    # Check trackers for other issues
     for tracker in torrent.trackers:
         if tracker.msg != 'This torrent is private' and tracker.status == 4 and tracker.msg not in unregistered:
             tracker_short = urlsplit(tracker.url)
-            print(torrent.name,' ',tracker.msg,' ',tracker_short.netloc)
-            client.torrents_add_tags(tags=["issue"],torrent_hashes=[torrent.hash])
+            print(torrent.name, ' ', tracker.msg, ' ', tracker_short.netloc)
+            
+            # Add a tag to the torrent
+            client.torrents_add_tags(tags=["issue"], torrent_hashes=[torrent.hash])
