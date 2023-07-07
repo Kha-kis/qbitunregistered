e04c9bb9866f9564bb4695a57dd61c6b0fedf6fa
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-07 11:52:26 -0500

    Update qbitunregistered.py

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -48,10 +48,6 @@ logging.info("Fetching torrent information from qBittorrent...")
 torrents = client.torrents.info()
 logging.info("Total torrents found: %d", len(torrents))
 
-# Log the total number of each tag
-logging.info("Tag statistics:")
-tag_counts = {tag: 0 for tag in set(config.unregistered + [config.other_issues_tag])}
-
 # Iterate through all the torrents
 for torrent in client.torrents.info():
     
@@ -108,15 +104,6 @@ for torrent in client.torrents.info():
             else:
                 # Not a dry run, execute the action
                 client.torrents_add_tags(tags=tags_to_add, torrent_hashes=[torrent.hash])
-    # Update tag counts for the current torrent
-    tags = torrent.tags
-    for tag in tags:
-        if tag in tag_counts:
-            tag_counts[tag] += 1
-
-# Log the total number of each tag
-for tag, count in tag_counts.items():
-    logging.info("Total torrents with tag '%s': %d", tag, count)
 
 # Log script end
 logging.info("qbitunregistered script completed.")
