0e14837b38c2fb817a58563ace84488d57ad52ae
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-07 13:04:10 -0500

    Update qbitunregistered.py
    adding scheduler

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -20,6 +20,7 @@ parser.add_argument('--username', type=str, help='The username for logging into 
 parser.add_argument('--password', type=str, help='The password for logging into qBittorrent Web UI.')
 parser.add_argument('--dry-run', action='store_true', help='If set, the script will only print actions without executing them.')
 parser.add_argument('--other-issues-tag', type=str, help='The tag to be used for torrents that have issues other than being unregistered.')
+parser.add_argument('--run-scheduler', action='store_true', help='If set, run the scheduler script.')
 
 # Parse command-line arguments
 args = parser.parse_args()
@@ -30,101 +31,101 @@ username = args.username if args.username else config.username
 password = args.password if args.password else config.password
 dry_run = args.dry_run if args.dry_run else config.dry_run
 other_issues_tag = args.other_issues_tag if args.other_issues_tag else config.other_issues_tag
-unregistered = config.unregistered
 
-# Connect to qBittorrent client
-client = Client(host=config.host, username=config.username, password=config.password)
+# Check if the --run-scheduler flag is specified and scheduler is enabled in the config
+if args.run_scheduler and config.enable_scheduler:
+    subprocess.run(["python", "scheduler.py"])  # Replace with the appropriate command to run scheduler.py
+else:
+    # Connect to qBittorrent client
+    client = Client(host=host, username=username, password=password)
 
-# Log script start
-logging.info("Starting qbitunregistered script...")
+    # Log script start
+    logging.info("Starting qbitunregistered script...")
 
-# List of unregistered tracker messages
-unregistered = config.unregistered
+    # List of unregistered tracker messages
+    unregistered = config.unregistered
 
-# Dictionary to store file paths and their associated hashes
-torrent_file_paths = {}
+    # Dictionary to store file paths and their associated hashes
+    torrent_file_paths = {}
 
-# Get all torrents from qBittorrent
-logging.info("Fetching torrent information from qBittorrent...")
-torrents = client.torrents.info()
-logging.info("Total torrents found: %d", len(torrents))
+    # Get all torrents from qBittorrent
+    logging.info("Fetching torrent information from qBittorrent...")
+    torrents = client.torrents.info()
+    logging.info("Total torrents found: %d", len(torrents))
 
-# Iterate through all the torrents
-for torrent in client.torrents.info():
-    
-    # Store the hashes in the torrent_file_paths dictionary
-    if torrent.save_path not in torrent_file_paths:
-        torrent_file_paths[torrent.save_path] = [torrent.hash]
-    else:
-        torrent_file_paths[torrent.save_path].append(torrent.hash)
+    # Iterate through all the torrents
+    for torrent in client.torrents.info():
 
-    # Initialize unregistered_count to 0
-    unregistered_count = 0
-
-    # Check trackers for unregistered messages
-    for tracker in torrent.trackers:
-        is_unregistered = False
-        # Check if the message matches any pattern in the unregistered list
-        for pattern in unregistered:
-            lower_pattern = pattern.lower()
-            lower_msg = tracker.msg.lower()
-            if lower_pattern.startswith("starts_with:") and lower_msg.startswith(lower_pattern.split("starts_with:")[1]):
-                is_unregistered = True
-                break
-            elif lower_msg == lower_pattern:
-                is_unregistered = True
-                break
-        # If the message is unregistered, increment the counter and print info
-        if is_unregistered and tracker.status == 4:
-            unregistered_count += 1
-            tracker_short = urlsplit(tracker.url)
-            logging.info("%s %s %s", torrent.name, tracker.msg, tracker_short.netloc)
-
-    # Add tags based on unregistered_count
-    if unregistered_count > 0:
-        tags_to_add = ["unregistered:crossseeding"] if len(torrent_file_paths[torrent.save_path]) > 1 else ["unregistered"]
-        if config.dry_run:
-            # Dry run, only print what would be done
-            print(f"[Dry Run] Would add tags {tags_to_add} to torrent with hash {torrent.hash}")
+        # Store the hashes in the torrent_file_paths dictionary
+        if torrent.save_path not in torrent_file_paths:
+            torrent_file_paths[torrent.save_path] = [torrent.hash]
         else:
-            # Not a dry run, execute the action
-            client.torrents_add_tags(tags=tags_to_add, torrent_hashes=[torrent.hash])
-        continue
+            torrent_file_paths[torrent.save_path].append(torrent.hash)
 
-    # Check trackers for other issues
-    for tracker in torrent.trackers:
-        if tracker.msg != 'This torrent is private' and tracker.status == 4 and tracker.msg.lower() not in [p.lower() for p in unregistered]:
-            tracker_short = urlsplit(tracker.url)
-            logging.info("%s %s %s", torrent.name, tracker.msg, tracker_short.netloc)
-            
-            # Add a tag to the torrent
-            tags_to_add = [config.other_issues_tag]
-            if config.dry_run:
+        # Initialize unregistered_count to 0
+        unregistered_count = 0
+
+        # Check trackers for unregistered messages
+        for tracker in torrent.trackers:
+            is_unregistered = False
+            # Check if the message matches any pattern in the unregistered list
+            for pattern in unregistered:
+                lower_pattern = pattern.lower()
+                lower_msg = tracker.msg.lower()
+                if lower_pattern.startswith("starts_with:") and lower_msg.startswith(
+                        lower_pattern.split("starts_with:")[1]):
+                    is_unregistered = True
+                    break
+                elif lower_msg == lower_pattern:
+                    is_unregistered = True
+                    break
+            # If the message is unregistered, increment the counter and print info
+            if is_unregistered and tracker.status == 4:
+                unregistered_count += 1
+                tracker_short = urlsplit(tracker.url)
+                logging.info("%s %s %s", torrent.name, tracker.msg, tracker_short.netloc)
+
+        # Add tags based on unregistered_count
+        if unregistered_count > 0:
+            tags_to_add = ["unregistered:crossseeding"] if len(torrent_file_paths[torrent.save_path]) > 1 else [
+                "unregistered"]
+            if dry_run:
                 # Dry run, only print what would be done
-                logging.info("[Dry Run] Would add tags %s to torrent with hash %s", tags_to_add, torrent.hash)
+                print(f"[Dry Run] Would add tags {tags_to_add} to torrent with hash {torrent.hash}")
             else:
                 # Not a dry run, execute the action
                 client.torrents_add_tags(tags=tags_to_add, torrent_hashes=[torrent.hash])
-                
-    # Delete torrents and files based on delete_tags and delete_files configuration
-    for tag in config.delete_tags:
-        if tag in torrent.tags:
-            if config.delete_files.get(tag, False):
-                if not config.dry_run:
-                    # Delete files
-                    client.torrents.delete(torrent.hash, delete_files=True)
-                    logging.info("Deleted torrent '%s' with hash %s and its files.", torrent.name, torrent.hash)
-                else:
-                    # Dry run, only print what would be done
-                    logging.info("[Dry Run] Would delete torrent '%s' with hash %s and its files.", torrent.name, torrent.hash)
-            else:
-                if not config.dry_run:
-                    # Delete torrent without files
-                    client.torrents.delete(torrent.hash, delete_files=False)
-                    logging.info("Deleted torrent '%s' with hash %s.", torrent.name, torrent.hash)
-                else:
-                    # Dry run, only print what would be done
-                    logging.info("[Dry Run] Would delete torrent '%s' with hash %s.", torrent.name, torrent.hash)
+            continue
 
-# Log script end
-logging.info("qbitunregistered script completed.")
+        # Check trackers for other issues
+        for tracker in torrent.trackers:
+            if tracker.msg != 'This torrent is private' and tracker.status == 4 and tracker.msg.lower() not in [
+                p.lower() for p in unregistered]:
+                tracker_short = urlsplit(tracker.url)
+                logging.info("%s %s %s", torrent.name, tracker.msg, tracker_short.netloc)
+
+                # Add a tag to the torrent
+                tags_to_add = [other_issues_tag]
+                if dry_run:
+                    # Dry run, only print what would be done
+                    logging.info("[Dry Run] Would add tags %s to torrent with hash %s", tags_to_add, torrent.hash)
+                else:
+                    # Not a dry run, execute the action
+                    client.torrents_add_tags(tags=tags_to_add, torrent_hashes=[torrent.hash])
+
+        # Delete torrents and files based on delete_tags and delete_files configuration
+        for tag in config.delete_tags:
+            if tag in torrent.tags:
+                delete_files = config.delete_files.get(tag, False)
+                if not dry_run:
+                    # Delete torrent and files if specified
+                    client.torrents.delete(torrent.hash, delete_files=delete_files)
+                    logging.info("Deleted torrent '%s' with hash %s%s.", torrent.name, torrent.hash,
+                                 " and its files" if delete_files else "")
+                else:
+                    # Dry run, only print what would be done
+                    logging.info("[Dry Run] Would delete torrent '%s' with hash %s%s.", torrent.name, torrent.hash,
+                                 " and its files" if delete_files else "")
+
+    # Log script end
+    logging.info("qbitunregistered script completed.")
