c114054f9627a615788c1859089493569bb08187
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 16:04:10 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from tqdm import tqdm
 
 def find_orphaned_files(client):
     logging.info("Checking for orphaned files...")
@@ -14,12 +15,16 @@ def find_orphaned_files(client):
             save_paths.add(save_path)
 
     # Traverse the save paths and find orphaned files
-    for save_path in save_paths:
-        for root, dirs, files in os.walk(save_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                if not any(torrent.content_path == file_path for torrent in client.torrents.info()):
-                    orphaned_files.append(file_path)
+    total_save_paths = len(save_paths)
+    with tqdm(total=total_save_paths, desc="Progress") as pbar:
+        for save_path in save_paths:
+            for root, dirs, files in os.walk(save_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    if not any(torrent.content_path == file_path for torrent in client.torrents.info()):
+                        orphaned_files.append(file_path)
+
+            pbar.update(1)
 
     # Log the orphaned files count and return the list of orphaned files
     logging.info("Total orphaned files: %d", len(orphaned_files))
