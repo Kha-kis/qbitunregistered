f1420cc3b6dd92a4b76d3edea92fde380d3cd9cb
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 16:24:42 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,9 +1,46 @@
 import os
 
-def get_root_files(directory):
-    root_files = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            root_files.append(file_path)
-    return root_files
+def check_files_on_disk(client):
+    # Retrieve the save paths from qBittorrent
+    save_paths = client.preferences.save_path
+
+    # Iterate over the save paths and check for orphaned files
+    for save_path in save_paths:
+        # Get the list of files/directories in the save path
+        files_on_disk = os.listdir(save_path)
+
+        # Fetch the list of torrents from qBittorrent
+        torrents = client.torrents.info()
+
+        # Check each file in the save path
+        for file in files_on_disk:
+            # Check if the file is orphaned
+            if is_orphaned(file, torrents):
+                # File is orphaned, perform actions
+                process_orphaned_file(save_path, file)
+
+def is_orphaned(file, torrents):
+    # Check if the file is orphaned based on the list of torrents
+    for torrent in torrents:
+        if torrent.content_path == file:
+            # File is associated with a torrent, not orphaned
+            return False
+    
+    # File is orphaned
+    return True
+
+def process_orphaned_file(save_path, file):
+    # Perform actions for orphaned file
+    # For example, you can print the file path
+    print(f"Orphaned file: {os.path.join(save_path, file)}")
+
+# Example usage
+if __name__ == "__main__":
+    # Connect to qBittorrent
+    client = Client("http://localhost:8080/")
+
+    # Set the API credentials if needed
+    # client.login("username", "password")
+
+    # Call the check_files_on_disk function
+    check_files_on_disk(client)
