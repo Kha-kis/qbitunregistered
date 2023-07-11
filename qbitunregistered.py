70574298b75d88bd5143d31456715a93093a3a0b
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 11:42:48 -0500

    updated to use config.json

--- qbitunregistered.py
+++ qbitunregistered.py
@@ -1,84 +1,53 @@
 #!/usr/bin/python3
-import config
+import json
 import argparse
-from qbittorrentapi import Client
+import os
 import logging
+from qbittorrentapi import Client
 from scripts.orphaned import check_files_on_disk
-from scripts.unregistered_checks import check_unregistered_torrents
+from scripts.unregistered_checks import unregistered_checks
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Manage torrents in qBittorrent by checking torrent tracker messages.")
-    parser.add_argument('--host', type=str, help='The host and port where qBittorrent is running.')
-    parser.add_argument('--username', type=str, help='The username for logging into qBittorrent Web UI.')
-    parser.add_argument('--password', type=str, help='The password for logging into qBittorrent Web UI.')
-    parser.add_argument('--dry-run', action='store_true', help='If set, the script will only print actions without executing them.')
-    parser.add_argument('--other-issues-tag', type=str, help='The tag to be used for torrents that have issues other than being unregistered.')
-    parser.add_argument('--use-delete-tags', type=bool, help='Flag for using delete_tags in the script.')
-    parser.add_argument('--use-delete-files', type=bool, help='Flag for using delete_files in the script.')
-    parser.add_argument('--orphaned', action='store_true', help='If set, check for orphaned files on disk.')
-    parser.add_argument('--unregistered', action='store_true', help='If set, checks for unregistered torrents.')
-    return parser.parse_args()
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Manage torrents in qBittorrent by checking torrent tracker messages.")
+parser.add_argument('--config', type=str, default='config.json', help='Path to the config.json file.')
+parser.add_argument('--orphaned', action='store_true', help='If set, check for orphaned files on disk.')
+parser.add_argument('--unregistered', action='store_true', help='If set, check for unregistered torrents.')
 
-def configure_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
+# Parse command-line arguments
+args = parser.parse_args()
 
-def connect_to_qbittorrent(host, username, password):
-    return Client(host=host, username=username, password=password)
+# Resolve the absolute path to the config.json file
+config_file_path = os.path.abspath(args.config)
 
-def fetch_torrent_information(client):
-    return client.torrents.info()
+# Load configuration from config.json
+with open(config_file_path, 'r') as config_file:
+    config = json.load(config_file)
 
-def process_unregistered_torrents(client, dry_run):
-    unregistered_counts_per_path = check_unregistered_torrents(client)
-    if config.use_delete_tags:
-        delete_tags = config.delete_tags
-        delete_files = config.delete_files
-        for torrent in client.torrents.info():
-            for tag in delete_tags:
-                if tag in torrent.tags:
-                    if config.use_delete_files and delete_files.get(tag, False):
-                        if not dry_run:
-                            client.torrents.delete(torrent.hash, delete_files=True)
-                            logging.info("Deleted torrent '%s' with hash %s and its files.", torrent.name, torrent.hash)
-                        else:
-                            logging.info("[Dry Run] Would delete torrent '%s' with hash %s and its files.", torrent.name, torrent.hash)
-                    else:
-                        if not dry_run:
-                            client.torrents.delete(torrent.hash, delete_files=False)
-                            logging.info("Deleted torrent '%s' with hash %s.", torrent.name, torrent.hash)
-                        else:
-                            logging.info("[Dry Run] Would delete torrent '%s' with hash %s.", torrent.name, torrent.hash)
-    return unregistered_counts_per_path
+# Connect to qBittorrent client
+client = Client(host=config['host'], username=config['username'], password=config['password'])
 
-def process_orphaned_files(client):
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# Log script start
+logging.info("Starting qbitunregistered script...")
+
+# Get all torrents from qBittorrent
+logging.info("Fetching torrent information from qBittorrent...")
+torrents = client.torrents.info()
+logging.info("Total torrents found: %d", len(torrents))
+
+# Call the unregistered_checks function if --unregistered argument is passed
+if args.unregistered:
+    torrent_file_paths, unregistered_counts_per_path = unregistered_checks(client, config['unregistered'], config['dry_run'], config['use_delete_tags'], config['use_delete_files'])
+
+# Call the check_files_on_disk function if --orphaned argument is passed
+if args.orphaned:
     check_files_on_disk(client)
 
-def main():
-    args = parse_arguments()
-    host = args.host if args.host else config.host
-    username = args.username if args.username else config.username
-    password = args.password if args.password else config.password
-    dry_run = args.dry_run if args.dry_run else config.dry_run
-    
-    configure_logging()
-    
-    client = connect_to_qbittorrent(host, username, password)
-    logging.info("Starting qbitunregistered script...")
-    
-    torrents = fetch_torrent_information(client)
-    logging.info("Total torrents found: %d", len(torrents))
-    
-    if args.unregistered:
-        unregistered_counts_per_path = process_unregistered_torrents(client, dry_run)
-    
-    if args.orphaned:
-        process_orphaned_files(client)
-    
-    logging.info("qbitunregistered script completed.")
-
-if __name__ == '__main__':
-    main()
+# Log script end
+logging.info("qbitunregistered script completed.")
