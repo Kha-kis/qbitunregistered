e55662572fd890b67100c539d0d5e344706a48c1
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 12:07:12 -0500

    update orphaned logic and refactor

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -2,7 +2,7 @@
 import os
 import logging
 
-def get_all_files_in_directory(directory):
+def get_files_in_directory(directory):
     # Get all files in a given directory.
     files = []
     for root, _, filenames in os.walk(directory):
@@ -10,27 +10,25 @@ def get_all_files_in_directory(directory):
             files.append(os.path.join(root, filename))
     return files
 
-def check_files_for_torrent(torrent, files_on_disk, counts_per_category):
-    # Check if each file in the given list is in the torrent's files.
-    for file in files_on_disk:
-        if file not in torrent.files:
-            save_path = torrent.save_path
-            if save_path not in counts_per_category:
-                counts_per_category[save_path] = 1
-            else:
-                counts_per_category[save_path] += 1
-            logging.info(f'File "{file}" is on disk but not in the client for save path "{save_path}"')
+def check_files_for_save_path(client, save_path):
+    # Check files on disk against torrents in the specified save path.
+    files_on_disk = get_files_in_directory(save_path)
+    torrents = client.torrents.info(save_path=save_path)
+    
+    for torrent in torrents:
+        for file in files_on_disk:
+            if file not in torrent.files:
+                logging.info(f'File "{file}" is on disk but not in the client for save path "{save_path}"')
 
 def check_files_on_disk(client):
-    # Check all files on disk against the files in each torrent.
-    counts_per_category = {}
+    # Check files on disk against torrents in each save path.
 
-    for torrent in client.torrents.info():
-        save_path = torrent.save_path
-        files_on_disk = get_all_files_in_directory(save_path)
-        check_files_for_torrent(torrent, files_on_disk, counts_per_category)
+    # Check default save path
+    default_save_path = client.app.default_save_path
+    check_files_for_save_path(client, default_save_path)
 
-    # Log counts per category and save path
-    for save_path, count in counts_per_category.items():
-        logging.info(f"Found {count} orphaned file(s) in save path: {save_path}")
-
+    # Check save path for each category
+    categories = client.torrents.categories()
+    for category in categories:
+        save_path = category.savePath
+        check_files_for_save_path(client, save_path)
