2e533814cd363853a3ee6a394598d98033e3d559
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 15:50:56 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -5,26 +5,31 @@ def check_files_on_disk(client):
     # Get the default save path from qBittorrent API
     default_save_path = client.app.default_save_path
 
-    # Get the category save paths
-    categories = client.torrents_categories().values()
-    category_save_paths = [category.savePath for category in categories if category.savePath != '']
-
-    # Combine the default save path and category save paths
-    save_paths = [default_save_path] + category_save_paths
-
-    # Collect all files and directories within the save paths
-    files_on_disk = set()
-    for save_path in save_paths:
-        files_on_disk.update(get_files_in_directory(save_path))
-
     # Get all torrents from qBittorrent
     torrents = client.torrents.info()
 
-    # Check if torrents are orphaned and log the file paths
-    for torrent in torrents:
-        content_path = torrent.content_path
-        if content_path not in files_on_disk:
-            logging.info(f"Orphaned file: {content_path}")
+    # Collect all files and directories within the default save path
+    files_on_disk = get_files_in_directory(default_save_path)
+
+    # Check if files on disk have corresponding torrents
+    orphaned_files = []
+    for file_path in files_on_disk:
+        found = False
+        for torrent in torrents:
+            content_path = torrent.content_path
+            if content_path and file_path.startswith(content_path):
+                found = True
+                break
+        if not found:
+            orphaned_files.append(file_path)
+
+    # Log the orphaned file paths
+    if orphaned_files:
+        logging.info("Orphaned files:")
+        for file_path in orphaned_files:
+            logging.info(file_path)
+    else:
+        logging.info("No orphaned files found.")
 
 def get_files_in_directory(directory):
     # Get all files and directories in a given directory
@@ -33,4 +38,3 @@ def get_files_in_directory(directory):
         for filename in filenames:
             files.add(os.path.join(root, filename))
     return files
-
