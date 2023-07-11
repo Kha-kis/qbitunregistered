5aefbcb45d7b1a309cc81210a567b4a0d3a416b5
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 14:33:36 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -11,7 +11,7 @@ def get_files_in_directory(directory):
     return files
 
 def check_files_on_disk(client):
-    # Check files on_disk against torrents in each save path.
+    # Check files on disk against torrents in each save path.
 
     # Get default save path from qBittorrent API
     default_save_path = client.app.default_save_path
@@ -20,51 +20,46 @@ def check_files_on_disk(client):
     save_paths = set()
     save_paths.add(default_save_path)
 
-    # Get all torrents
-    torrents = client.torrents.info()
-
-    # Add all unique save paths to set from torrents
-    for torrent in torrents:
-        save_paths.add(torrent.save_path)
-
-    # Check each unique save path
-    for save_path in save_paths:
-        print(f"Checking save path: {save_path}")
-
-        # Get files on_disk for current save path
-        files_on_disk = get_files_in_directory(save_path)
-
-        # Check each torrent in current save path
-        for torrent in torrents:
-            if torrent.save_path == save_path:
-                check_files_for_torrent(torrent, files_on_disk)
-
-
-
-
     # Get save paths for each category
     categories = client.torrents_categories().values()
     for category in categories:
         if category.savePath != '':
-            save_path = category.savePath
-            print(f"Checking save path: {save_path}")
-            files_on_disk = get_files_in_directory(save_path)
-            category_torrents = client.torrents.info(category=category.name)
-            for torrent in category_torrents:
-                if torrent.save_path == save_path:
-                    check_files_for_torrent(torrent, files_on_disk)
+            save_paths.add(category.savePath)
 
+    # Get all torrents
+    torrents = client.torrents.info()
 
-    # Get save paths for each category
-    categories = client.torrents_categories()
-    for category_name, category in categories.items():
-        if category.savePath != '':
-            save_path = category.savePath
-            files_on_disk = get_files_in_directory(save_path)
-            category_torrents = client.torrents.info(category=category_name)
-            for torrent in category_torrents:
-                if torrent.save_path == save_path:
-                    check_files_for_torrent(torrent, files_on_disk)
+    # Iterate over unique save paths
+    for save_path in save_paths:
+        logging.info(f"Checking save path: {save_path}")
+
+        # Get files on disk for current save path
+        files_on_disk = get_files_in_directory(save_path)
+
+        # Collect unregistered files per save path
+        unregistered_files = []
+
+        # Check each torrent in current save path
+        for torrent in torrents:
+            if torrent.save_path == save_path:
+                torrent_files = set(os.path.join(torrent.save_path, file['name']) for file in torrent.files)
+                unregistered_files.extend(file for file in files_on_disk if file not in torrent_files)
+
+        num_unregistered_files = len(unregistered_files)
+        logging.info(f"Total unregistered files: {num_unregistered_files}")
+
+        if num_unregistered_files > 0:
+            logging.info("Unregistered Files:")
+            # Set the maximum number of unregistered files to display per save path
+            max_display = 10
+            for file in unregistered_files[:max_display]:
+                logging.info(file)
+            if num_unregistered_files > max_display:
+                logging.info(f"and {num_unregistered_files - max_display} more unregistered files.")
+            logging.info("End of Unregistered Files")
+        else:
+            logging.info("No unregistered files found.")
+
 
 def check_files_for_torrent(torrent, files_on_disk):
     # Check if each file in the given list is in the torrent's files.
