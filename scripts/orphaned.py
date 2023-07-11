5967b0894b45f886c42dfcad00c99ce9e614c7d4
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 16:34:57 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,23 +1,30 @@
 import os
 
 def check_files_on_disk(client):
-    # Fetch the list of torrents from qBittorrent
-    torrents = client.torrents.info()
+    # Retrieve the save paths from qBittorrent
+    save_paths = [save_path.path for save_path in client.torrents.save_path()]
 
-    # Get unique save paths from the torrents
-    save_paths = set(torrent.save_path for torrent in torrents)
+    # Initialize the set to store orphaned files
+    orphaned_files = set()
 
     # Iterate over the save paths and check for orphaned files
     for save_path in save_paths:
         # Get the list of files/directories in the save path
         files_on_disk = os.listdir(save_path)
 
+        # Fetch the list of torrents from qBittorrent
+        torrents = client.torrents.info()
+
         # Check each file in the save path
         for file in files_on_disk:
             # Check if the file is orphaned
             if is_orphaned(file, torrents):
-                # File is orphaned, perform actions
-                process_orphaned_file(save_path, file)
+                # File is orphaned, add it to the set
+                orphaned_files.add(os.path.join(save_path, file))
+
+    # Return the set of orphaned files
+    return orphaned_files
+
 
 def is_orphaned(file, torrents):
     # Check if the file is orphaned based on the list of torrents
