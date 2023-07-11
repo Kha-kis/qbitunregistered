995cd085ac12c9afb4b3498fdcf6c7ef7a4cbc14
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 16:17:20 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,14 +1,9 @@
 import os
-import logging
 
-def check_files_on_disk(client):
-    # Get the list of root files
-    root_files = get_root_files()
-
-    # Get the list of files associated with torrents
-    torrent_files = set(file['name'] for torrent in client.torrents.info() for file in torrent.files())
-
-    # Identify orphaned files
-    orphaned_files = set(root_files) - torrent_files
-
-    return orphaned_files
+def get_root_files(directory):
+    root_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            root_files.append(file_path)
+    return root_files
