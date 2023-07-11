b65a854286df6de8725afc6b921a0f1b95ca82dd
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 14:41:38 -0500

    asdf

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -2,14 +2,6 @@
 import os
 import logging
 
-def get_files_in_directory(directory):
-    # Get all files in a given directory.
-    files = set()
-    for root, _, filenames in os.walk(directory):
-        for filename in filenames:
-            files.add(os.path.join(root, filename))
-    return files
-
 def check_files_on_disk(client):
     # Check files on disk against torrents in each save path.
 
@@ -33,59 +25,42 @@ def check_files_on_disk(client):
     for save_path in save_paths:
         logging.info(f"Checking save path: {save_path}")
 
-        # Get files on disk for current save path
-        files_on_disk = get_files_in_directory(save_path)
+        # Get files and directories on disk for current save path
+        files_on_disk = set()
+        dirs_on_disk = set()
+        for root, dirs, filenames in os.walk(save_path):
+            files_on_disk.update([os.path.join(root, filename) for filename in filenames])
+            dirs_on_disk.update([os.path.join(root, directory) for directory in dirs])
 
-        # Collect unregistered files per save path
-        unregistered_files = []
+        # Collect unregistered files and directories per save path
+        unregistered_files = set()
+        unregistered_dirs = set()
 
         # Check each torrent in current save path
         for torrent in torrents:
             if torrent.save_path == save_path:
                 torrent_files = set(os.path.join(torrent.save_path, file['name']) for file in torrent.files)
-                unregistered_files.extend(file for file in files_on_disk if file not in torrent_files)
-
-        num_unregistered_files = len(unregistered_files)
-        logging.info(f"Total unregistered files: {num_unregistered_files}")
-
-        if num_unregistered_files > 0:
-            logging.info("Unregistered Files:")
-            # Set the maximum number of unregistered files to display per save path
-            max_display = 10
-            for file in unregistered_files[:max_display]:
-                logging.info(file)
-            if num_unregistered_files > max_display:
-                logging.info(f"and {num_unregistered_files - max_display} more unregistered files.")
-            logging.info("End of Unregistered Files")
-        else:
-            logging.info("No unregistered files found.")
-
-
-def check_files_for_torrent(torrent, files_on_disk):
-    # Check if each file in the given list is in the torrent's files.
-    torrent_files = set(os.path.join(torrent.save_path, file['name']) for file in torrent.files)
-    files_on_disk_set = set(files_on_disk)
-
-    logging.info(f"Checking files for torrent '{torrent.name}' in save path '{torrent.save_path}'")
-    logging.info(f"Total files on disk: {len(files_on_disk_set)}")
-    logging.info(f"Total files associated with torrent: {len(torrent_files)}")
-
-    unregistered_files = files_on_disk_set - torrent_files
-    num_unregistered_files = len(unregistered_files)
-
-    logging.info(f"Total unregistered files: {num_unregistered_files}")
-
-    if num_unregistered_files > 0:
-        logging.info("Unregistered Files:")
-        for file in unregistered_files:
-            logging.info(file)
+                unregistered_files.update(file for file in files_on_disk if file not in torrent_files)
 
         # Check for unregistered directories
-        unregistered_dirs = set(os.path.dirname(file) for file in unregistered_files)
-        for dir_path in unregistered_dirs:
-            if not os.listdir(dir_path):
-                logging.info(f"Orphaned directory found: {dir_path}")
+        unregistered_dirs = dirs_on_disk - set(os.path.dirname(file) for file in unregistered_files)
+        unregistered_dirs = set(dir_path for dir_path in unregistered_dirs if not os.listdir(dir_path))
 
-        logging.info("End of Unregistered Files")
-    else:
-        logging.info("No unregistered files found.")
+        num_unregistered_files = len(unregistered_files)
+        num_unregistered_dirs = len(unregistered_dirs)
+
+        logging.info(f"Total unregistered files: {num_unregistered_files}")
+        logging.info(f"Total unregistered directories: {num_unregistered_dirs}")
+
+        if num_unregistered_files > 0 or num_unregistered_dirs > 0:
+            logging.info("Unregistered Files:")
+            for file in unregistered_files:
+                logging.info(file)
+
+            logging.info("Unregistered Directories:")
+            for directory in unregistered_dirs:
+                logging.info(directory)
+
+            logging.info("End of Unregistered Files and Directories")
+        else:
+            logging.info("No unregistered files or directories found.")
