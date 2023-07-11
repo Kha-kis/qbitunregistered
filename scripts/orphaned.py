de3a605adefee07c7808752c191d8a225c8d419e
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 12:38:42 -0500

    wtf2

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,80 +1,38 @@
-import os
+#!/usr/bin/python3
 import json
+import argparse
 import logging
 from qbittorrentapi import Client
+from scripts.orphaned import check_files_on_disk
 
-def get_files_in_directory(directory):
-    # Get all files within the specified save paths.
-    files = set()
-    for root, _, filenames in os.walk(directory):
-        for filename in filenames:
-            file_path = os.path.join(root, filename)
-            if file_path.startswith(directory):
-                files.add(file_path)
-    return files
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Manage torrents in qBittorrent by checking torrent tracker messages.")
+parser.add_argument('--config', type=str, default='config.json', help='Path to the config.json file.')
+parser.add_argument('--orphaned', action='store_true', help='If set, check for orphaned files on disk.')
 
-def check_files_on_disk(client, config):
-    # Check files on disk against torrents in each save path.
+# Parse command-line arguments
+args = parser.parse_args()
 
-    # Get default save path
-    default_save_path = client.app.default_save_path
-    default_files_on_disk = get_files_in_directory(default_save_path)
+# Load configuration from config.json
+with open(args.config, 'r') as config_file:
+    config = json.load(config_file)
 
-    # Check default save path for torrents without category
-    torrents = client.torrents.info(category=None)
-    for torrent in torrents:
-        if torrent.save_path == default_save_path:
-            check_files_for_torrent(torrent, default_files_on_disk)
+# Connect to qBittorrent client
+client = Client(host=config['host'], port=config['port'], username=config['username'], password=config['password'])
 
-    # Get save paths for each category
-    categories = client.torrents.categories()
-    for category in categories:
-        save_path = category.savePath
-        category_files_on_disk = get_files_in_directory(save_path)
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-        # Check save path for torrents in the category
-        torrents = client.torrents.info(category=category.name)
-        for torrent in torrents:
-            if torrent.save_path == save_path:
-                check_files_for_torrent(torrent, category_files_on_disk)
+# Log script start
+logging.info("Starting qbitunregistered script...")
 
-def check_files_for_torrent(torrent, files_on_disk):
-    # Check if each file in the given list is in the torrent's files.
-    for file in files_on_disk:
-        if file not in torrent.files:
-            logging.info(f'File "{file}" is on disk but not in the client for save path "{torrent.save_path}"')
-
-def run_orphaned_check(config_path):
-    # Load configuration from config.json
-    with open(config_path, 'r') as config_file:
-        config = json.load(config_file)
-
-    # Extract qBittorrent server information from config
-    host = config['host']
-    port = config['port']
-    username = config['username']
-    password = config['password']
-
-    # Connect to qBittorrent client
-    client = Client(host=host, port=port, username=username, password=password)
-
-    # Configure logging
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
-    # Log script start
-    logging.info("Starting orphaned check...")
-
-    # Check files on disk against torrents
+# Call the check_files_on_disk function if --orphaned argument is passed
+if args.orphaned:
     check_files_on_disk(client, config)
 
-    # Log script end
-    logging.info("Orphaned check completed.")
-
-# Example usage
-if __name__ == '__main__':
-    config_path = 'config.json'
-    run_orphaned_check(config_path)
+# Log script end
+logging.info("qbitunregistered script completed.")
