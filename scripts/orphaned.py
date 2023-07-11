5b2a67c25448f1ea6dbf8c2840af092024c70992
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-11 16:46:31 -0500

    damn

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -1,20 +1,11 @@
 import os
 
 def check_files_on_disk(client):
-    # Retrieve the save paths from torrent categories
-    category_save_paths = [category["savePath"] for category in client.torrents_categories() if category["savePath"]]
-
-    # Retrieve the save path from qBittorrent configuration
-    qbit_save_path = client.app.preferences().get("save_path")
-
-    # Combine the save paths from categories and configuration
-    save_paths = category_save_paths + [qbit_save_path] if qbit_save_path else category_save_paths
-
-    # Initialize the set to store orphaned files
-    orphaned_files = set()
+    # Retrieve the save paths from qBittorrent
+    category_save_paths = [category for category in client.torrents_categories() if category]
 
     # Iterate over the save paths and check for orphaned files
-    for save_path in save_paths:
+    for save_path in category_save_paths:
         # Get the list of files/directories in the save path
         files_on_disk = os.listdir(save_path)
 
@@ -25,24 +16,18 @@ def check_files_on_disk(client):
         for file in files_on_disk:
             # Check if the file is orphaned
             if is_orphaned(file, torrents):
-                # File is orphaned, add it to the set
-                orphaned_files.add(os.path.join(save_path, file))
-
-    # Return the set of orphaned files
-    return orphaned_files
-
-
+                # File is orphaned, perform actions
+                process_orphaned_file(save_path, file)
 
 def is_orphaned(file, torrents):
     # Check if the file is orphaned based on the list of torrents
     for torrent in torrents:
-        if any(file.startswith(content_path) for content_path in torrent.content_path):
+        if torrent.content_path == file:
             # File is associated with a torrent, not orphaned
             return False
     
     # File is orphaned
     return True
-
 
 def process_orphaned_file(save_path, file):
     # Perform actions for orphaned file
