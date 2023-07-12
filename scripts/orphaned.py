ecfe4caee095a1b8911f165c73d3e6db263cf446
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-12 09:05:30 -0500

    fixed api endpoints

--- scripts/orphaned.py
+++ scripts/orphaned.py
@@ -10,26 +10,27 @@ def list_directory_files(path: str) -> List[str]:
             file_list.append(os.path.join(root, file))
     return file_list
 
-def check_files_on_disk(client, torrents):
-    """Check for orphaned files on disk"""
-    # Get default save path
-    default_save_path = client.application.default_save_path
-
-    # Identify all save paths
+def check_files_on_disk(client, torrents: List) -> List[str]:
+    # Get the default save path
+    default_save_path = client.application.defaultSavePath
     save_paths = set([default_save_path])
-    categories = client.categories.info()
+
+    # Get the categories and their save paths
+    categories = client.torrent_categories.categories
+
+    # Add the save path for each category to the set of save paths
     for category in categories.values():
         save_paths.add(category['savePath'])
 
-    # Identify all torrent associated files
-    torrent_files = [file['content_path'] for torrent in torrents for file in torrent.files()]
+    # Identify all torrent-associated files
+    torrent_files = [f['content_path'] for torrent in torrents for f in torrent.files()]
 
-    # Identify all files in the save paths
+    # Find all files and folders in each save path
     all_files = []
     for path in save_paths:
-        all_files += list_directory_files(path)
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                all_files.append(os.path.join(root, name))
 
-    # Identify orphaned files
-    orphaned_files = set(all_files) - set(torrent_files)
-
-    return orphaned_files
+    # Identify orphaned files: those that exist in the file system but not in the list of torrent-associated files
+    orphaned_files = [f for f in all_files if f not in torrent_files]
