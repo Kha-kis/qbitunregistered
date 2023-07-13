bb012c2b1a1dde068a009d4fa4a859cf9d7072da
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-13 08:41:09 -0500

    updated logic for tracker tagging. added seeding_management.

--- scripts/seeding_management.py
+++ scripts/seeding_management.py
@@ -1,27 +1,30 @@
+from qbittorrentapi import Client
 import logging
 
-def tag_by_tracker(client, torrents, config):
+def apply_seed_time(client, config):
+    torrents = client.torrents.info()
+
     for torrent in torrents:
         for tracker in torrent.trackers:
             tracker_tag_config = config.get('tracker_tags', {}).get(tracker.host)
 
             if tracker_tag_config is not None:
-                tag = tracker_tag_config.get('tag')
                 seed_time_limit = tracker_tag_config.get('seed_time_limit')
-                seed_ratio_limit = tracker_tag_config.get('seed_ratio_limit')
 
-                # Add the tag to the torrent
-                client.torrents_add_tags(torrent_hashes=[torrent.hash], tags=[tag])
-                logging.info(f"Added tag '{tag}' to torrent with name '{torrent.name}'")
-
-                # Apply seed time limit if provided
                 if seed_time_limit is not None:
                     client.torrents_edit(torrent.hash, seeding_time_limit=seed_time_limit)
                     logging.info(f"Updated seeding time limit for torrent with name '{torrent.name}' to {seed_time_limit} minutes.")
 
-                # Apply seed ratio limit if provided
+def apply_seed_ratio(client, config):
+    torrents = client.torrents.info()
+
+    for torrent in torrents:
+        for tracker in torrent.trackers:
+            tracker_tag_config = config.get('tracker_tags', {}).get(tracker.host)
+
+            if tracker_tag_config is not None:
+                seed_ratio_limit = tracker_tag_config.get('seed_ratio_limit')
+
                 if seed_ratio_limit is not None:
                     client.torrents_edit(torrent.hash, ratio_limit=seed_ratio_limit)
                     logging.info(f"Updated seed ratio limit for torrent with name '{torrent.name}' to {seed_ratio_limit}.")
-
-    logging.info("Tagging by tracker completed.")
