0bc0a59ba60fbd350c95c779582eca5bc31af154
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-13 17:17:23 -0500

    updated logging output to be a summary for rausing or resuming torrents

--- scripts/torrent_management.py
+++ scripts/torrent_management.py
@@ -2,11 +2,17 @@ import logging
 from qbittorrentapi import Client
 
 def pause_torrents(client, torrents):
+    total_paused = 0
     for torrent in torrents:
         client.torrents_pause(torrent.hash)
-        logging.info(f"Paused torrent with name '{torrent.name}'")
+        total_paused += 1
+
+    logging.info("Paused %d torrents.", total_paused)
 
 def resume_torrents(client, torrents):
+    total_resumed = 0
     for torrent in torrents:
         client.torrents_resume(torrent.hash)
-        logging.info(f"Resumed torrent with name '{torrent.name}'")
+        total_resumed += 1
+
+    logging.info("Resumed %d torrents.", total_resumed)
