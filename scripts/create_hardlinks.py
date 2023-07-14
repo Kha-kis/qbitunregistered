9f7bc0bcf73fec197e72e7e241b13a63c60f79db
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-14 13:59:48 -0500

    try again with hardlink logic

--- scripts/create_hardlinks.py
+++ scripts/create_hardlinks.py
@@ -2,12 +2,20 @@ import os
 
 def create_hard_links(target_dir, torrents):
     for torrent in torrents:
-        if torrent.state_enum.is_complete:
-            source_path = torrent.save_path
-            target_path = os.path.join(target_dir, torrent.name)
+        if torrent.state_enum.is_completed:
+            content_path = os.path.join(torrent.save_path, torrent.name)
 
-            try:
-                os.link(source_path, target_path)
-                print(f"Hard link created for torrent '{torrent.name}'")
-            except OSError as e:
-                print(f"Failed to create hard link for torrent '{torrent.name}': {str(e)}")
+            if os.path.isdir(content_path):
+                for root, dirs, files in os.walk(content_path):
+                    for file in files:
+                        source_path = os.path.join(root, file)
+                        target_path = os.path.join(target_dir, torrent.category or '', file)
+
+                        try:
+                            os.makedirs(os.path.dirname(target_path), exist_ok=True)
+                            os.link(source_path, target_path)
+                            print(f"Hard link created for file '{source_path}'")
+                        except OSError as e:
+                            print(f"Failed to create hard link for file '{source_path}': {str(e)}")
+            else:
+                print(f"Skipping non-directory torrent '{torrent.name}'")
