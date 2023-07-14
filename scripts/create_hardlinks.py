613fc082df3b8cf8dfd34aa5b0ce7ccecea502a6
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-14 14:02:51 -0500

    add check if HL already exist

--- scripts/create_hardlinks.py
+++ scripts/create_hardlinks.py
@@ -11,11 +11,14 @@ def create_hard_links(target_dir, torrents):
                         source_path = os.path.join(root, file)
                         target_path = os.path.join(target_dir, torrent.category or '', file)
 
-                        try:
-                            os.makedirs(os.path.dirname(target_path), exist_ok=True)
-                            os.link(source_path, target_path)
-                            print(f"Hard link created for file '{source_path}'")
-                        except OSError as e:
-                            print(f"Failed to create hard link for file '{source_path}': {str(e)}")
+                        if not os.path.exists(target_path):
+                            try:
+                                os.makedirs(os.path.dirname(target_path), exist_ok=True)
+                                os.link(source_path, target_path)
+                                print(f"Hard link created for file '{source_path}'")
+                            except OSError as e:
+                                print(f"Failed to create hard link for file '{source_path}': {str(e)}")
+                        else:
+                            print(f"Hard link already exists for file '{source_path}'")
             else:
                 print(f"Skipping non-directory torrent '{torrent.name}'")
