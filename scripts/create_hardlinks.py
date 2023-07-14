c7e4e1d821523d880be5091fd9f1c09d68cd7e63
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-07-14 13:32:53 -0500

    added creation of hardlinks of compelted torrents

---
+++ scripts/create_hardlinks.py
@@ -0,0 +1,14 @@
+import os
+
+
+def create_hard_links(target_dir, torrents):
+    for torrent in torrents:
+        if torrent.state_enum.is_completed:
+            source_path = torrent.save_path
+            target_path = os.path.join(target_dir, torrent.name)
+
+            try:
+                os.link(source_path, target_path)
+                print(f"Hard link created for torrent '{torrent.name}'")
+            except OSError as e:
+                print(f"Failed to create hard link for torrent '{torrent.name}': {str(e)}")
